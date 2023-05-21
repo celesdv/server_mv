d73a4134c9a379607518079b4bf321dbbafe2d60
Celeste Dangelo <celesdv@gmail.com>
2023-05-21 13:26:24 -0300

    fix .env DB_HOST

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER || "root",
   process.env.DB_PASSWORD || "root",
   {
-    host: process.env.DB_PORT || "localhost",
+    host: process.env.DB_HOST || "localhost",
     dialect: "mysql",
   }
 );
