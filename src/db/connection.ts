db7d2db88564ca2809ce94ccaf574665bfd42bec
Celeste Dangelo <celesdv@gmail.com>
2023-05-21 13:56:03 -0300

    DB_PORT added

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || "root",
   {
     host: process.env.DB_HOST || "localhost",
+    port: Number(process.env.DB_PORT) || 3306,
     dialect: "mysql",
   }
 );
